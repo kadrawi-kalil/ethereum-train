d01bb660ce4ad034fb24858bb4432cf711d17f6f
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-02-22 11:15:50 +0100

    addOne && selectors && doNothing

--- 1.9.Storage.sol
+++ 1.9.Storage.sol
@@ -2,21 +2,19 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-
 contract Storage {
+    uint256 private a = 10;
+    uint256 private b = 20;
+    uint256 private c = 30;
 
-    uint256 private a=10;
-    uint256 private b=20;
-    uint256 private c=30;
-
-  
-    function getAtSlot(uint256 slot) external pure  returns(uint256) {
+    function getAtSlot(uint256 slot) external pure returns (uint256) {
         uint256 value;
 
         assembly {
-         //   value:=sload(slot)
-         value:=b.slot
+            //   value:=sload(slot)
+            value := b.slot
         }
-        
+
         return value;
-    }}
+    }
+}
