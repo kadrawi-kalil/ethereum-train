baad9d8067c8b235bc1106eb0a80c12a24b3a6b7
khadhraouikhalil <khadhraouikhalil52@gmail.com>
2023-03-24 12:36:52 +0100

    add comment to code

--- advanced-solidity-gas-cost/part2-storage/StorageExample.sol
+++ advanced-solidity-gas-cost/part2-storage/StorageExample.sol
@@ -5,10 +5,27 @@ contract StorageExample {
     uint256 private myInteger=0;
 
     function setToOne() external {
+        // 26 266
+        //-21 000 trx
+        //- 5 000 storage
+        //_________
+        //=   266
+        //(- 2 100) no cold acces fee (because it is not the first time)
+        //-   100  gas if it the variable has already been touched
+        //________
+        //=    166 =>cost calling every function even it is empty
         myInteger=1;
     }
 
     function setToTwo() external {
+        // 43 300
+        //-21 000 trx
+        //-20 000 storage  (because 0 to non zero)
+        //_________
+        //= 2 300
+        //- 2 100 cold acces fee
+        //________
+        //=   200 =>cost calling every function even it is empty
         myInteger=2;
     }
 
@@ -21,7 +38,13 @@ contract StorageExample {
     }
 
     function getAndSetInteger() external {
+        // 43 545
         uint256 _myInteger = myInteger;
+        //- 2 100 (read +cold acces)
         myInteger= _myInteger +1;
+        //-20 100 (write+warm acces)
     }
+
+    //Cost(Read + Write)=~Cost(Write)
+}
 
